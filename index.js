eb7f8f9600047534d22926b66c9602f572e8c9e7
konstantin <makarovestates@gmail.com>
2021-12-19 22:44:48 -0600

    Second attempt

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+    if(feet <= 400) {
+          return 'This one is on me!'
+    } else if(feet > 2000 && feet <= 2500){
+          return 'I will gladly take your thirty bucks.'
+    } else if(feet > 2500){
+          return 'No can do.'
+    }
+       
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function ternaryCheckCity(city){
+    return (city == 'NYC') ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(answer){
+      let tipGenerosity;
+
+      switch (answer){
+            case (tipGenerosity = 'generous'): answer = 'Thank you so much.';
+            break;
+
+            case (tipGenerosity = 'not so generous'): answer = 'Thank you.';
+            break;
+
+            default: answer = 'Bye.';
+      }
 }
+
+
+      
